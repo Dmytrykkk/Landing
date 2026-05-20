7594e27d7369f744cfb80babc11a6aca85a90d96
Dmytrykkk <125646280+Dmytrykkk@users.noreply.github.com>
2026-05-20 23:57:25 +0300

    Add background color to main content section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -203,7 +203,7 @@ export default function HomePage() {
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(faqPageSchema) }}
       />
-      <main id="main-content" tabIndex={-1} className="">
+      <main id="main-content" tabIndex={-1} className="bg-[#1e2939]">
         <ScrollTracker />
         <Hero />
         <SectionReveal>
